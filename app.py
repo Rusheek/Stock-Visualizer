0931e429f7013f39fd22dd3e0918f832a3738f9d
rusheekt@gmail.com <rusheekt@gmail.com>
2025-04-25 19:11:33 +0530

    added graphs logic

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ import streamlit as st
 import logic
 import logic.load_data
 from utils import global_data as gd
+import datetime
 
 logic.load_data.set_global_data()
-# print(gd.sectors)
-# print(gd.nifty_df)
+
+gd.start_date = st.date_input('Enter Start Date',min_value=datetime.date(2020,1,1),value=datetime.date(2020,1,1))
+gd.end_date = st.date_input('Enter End Date',max_value='today',value='today')
+
+st.write('start date is :',gd.start_date)
+st.write('end date is :', gd.end_date)
 
 
 
---
+++ logic/graphs.py
@@ -0,0 +1,161 @@
+# Logic for all the graphs
+import streamlit as st
+import pandas_datareader.data as reader
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import mplfinance as mpf
+
+def relativeret(df):
+    daily_returns = df.pct_change()
+    daily_cum_returns = (daily_returns +1).cumprod()
+    daily_cum_returns = daily_cum_returns.fillna(0)
+    return daily_cum_returns
+
+def peer_line(sts):
+    dropdown = st.multiselect('Pick your assest',sts)
+    if len(dropdown)>0:
+        df = relativeret(reader.get_data_yahoo(dropdown,start,end)['Adj Close'])
+        #st.write(df)
+        i=0
+        fig = go.Figure()
+        st.write(dropdown)
+        for x in dropdown:
+            fig.add_traces(go.Scatter(x=df.index,y=df[x],mode ='lines',name=x))
+
+        st.plotly_chart(fig)
+
+def Line_chart(df):
+     
+    st.line_chart(data=df)
+
+def interactive_candelsticks(df1):
+    fig = go.Figure(data =[go.Candlestick(x=df1.index,
+                                     open=df1['Open'],
+                                     high = df1['High'],
+                                     low = df1['Low'],
+                                     close = df1['Close'])])
+
+    st.plotly_chart(fig)
+
+
+def mpf_plots(df):
+    #
+    fig=mpf.plot(df, type='candle', style='yahoo',
+        title='Moving Averages',
+        ylabel='Price',
+        ylabel_lower='',
+        volume=True,
+        mav=(50,200),tight_layout=True,figscale=0.75
+
+       )
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(fig)
+    #st.plt.legend()
+
+#RSI
+def get_RSI(df, column='Adj Close', time_window=14):
+    """Return the RSI indicator for the specified time window."""
+    diff = df[column].diff(1)
+
+    # This preservers dimensions off diff values.
+    up_chg = 0 * diff
+    down_chg = 0 * diff
+
+    # Up change is equal to the positive difference, otherwise equal to zero.
+    up_chg[diff > 0] = diff[diff > 0]
+
+    # Down change is equal to negative deifference, otherwise equal to zero.
+    down_chg[diff < 0] = diff[diff < 0]
+
+    # We set com = time_window-1 so we get decay alpha=1/time_window.
+    up_chg_avg = up_chg.ewm(com=time_window - 1,
+                            min_periods=time_window).mean()
+    down_chg_avg = down_chg.ewm(com=time_window - 1,
+                                min_periods=time_window).mean()
+
+    RS = abs(up_chg_avg / down_chg_avg)
+    df['RSI'] = 100 - 100 / (1 + RS)
+
+    return df
+
+
+def plot_volume(fig, df, row, column=1):
+    """Return a graph object figure containing the volume chart in the specified row."""
+    fig.add_trace(go.Bar(x=df.index,
+                         y=df['Volume'],
+                         marker=dict(color='lightskyblue',
+                                     line=dict(color='firebrick', width=0.1)),
+                         showlegend=False,
+                         name='Volume'),
+                  row=row,
+                  col=column)
+
+    fig.update_xaxes(title_text='Date', row=row, col=column)
+    fig.update_yaxes(title_text='Volume ', row=row, col=column)
+
+    return fig
+
+
+def plot_RSI(fig, df, row, column=1):
+    """Return a graph object figure containing the RSI indicator in the specified row."""
+    df1=df.reset_index()
+    fig.add_trace(go.Scatter(x=df1['Date'].iloc[30:],
+                             y=df1['RSI'].iloc[30:],
+                             name='RSI',
+                             line=dict(color='gold', width=2)),
+                  row=row,
+                  col=column)
+
+    fig.update_yaxes(title_text='RSI', row=row, col=column)
+
+    # Add one red horizontal line at 70% (overvalued) and green line at 30% (undervalued)
+    for y_pos, color in zip([70, 30], ['Red', 'Green']):
+        fig.add_shape(x0=df1['Date'].iloc[1],
+                      x1=df1['Date'].iloc[-1],
+                      y0=y_pos,
+                      y1=y_pos,
+                      type='line',
+                      line=dict(color=color, width=2),
+                      row=row,
+                      col=column)
+
+    # Add a text box for each line
+    for y_pos, text, color in zip([64, 36], ['Overvalued', 'Undervalued'], ['Red', 'Green']):
+        fig.add_annotation(x=df1['Date'].iloc[int(df1['Date'].shape[0] / 10)],
+                           y=y_pos,
+                           text=text,
+                           font=dict(size=14, color=color),
+                           bordercolor=color,
+                           borderwidth=1,
+                           borderpad=2,
+                           bgcolor='lightsteelblue',
+                           opacity=0.75,
+                           showarrow=False,
+                           row=row,
+                           col=column)
+
+    # Update the y-axis limits
+    ymin = 25 if df1['RSI'].iloc[30:].min() > 25 else df1['RSI'].iloc[30:].min() - 5
+    ymax = 75 if df1['RSI'].iloc[30:].max() < 75 else df1['RSI'].iloc[30:].max() + 5
+    fig.update_yaxes(range=[ymin, ymax], row=row, col=column)
+
+    return fig
+
+
+def plot_candlestick_chart(fig, df, row, column=1):
+    """Return a graph object figure containing a Candlestick chart in the specified row."""
+    fig.add_trace(go.Candlestick(x=df.index,
+                                 open=df['Open'],
+                                 high=df['High'],
+                                 low=df['Low'],
+                                 close=df['Close'],
+                                 name='Candlestick Chart'),
+                  row=row,
+                  col=column)
+    fig.update_xaxes(rangeslider={'visible': False})
+    fig.update_yaxes(title_text='Price ', row=row, col=column)
+
+    return fig
+
+def bar_plot(df):
+    st.bar_chart(df)
